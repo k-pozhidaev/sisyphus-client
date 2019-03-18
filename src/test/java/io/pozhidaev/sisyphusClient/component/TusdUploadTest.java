d3a9d9547337657b007bc0c0439964a4fab42f95
Konstantin Pozhidaev <kos89jed@gmail.com>
2019-03-18 23:04:04 +0200

    Updated tests.

--- src/test/java/io/pozhidaev/sisyphusClient/component/TusdUploadTest.java
+++ src/test/java/io/pozhidaev/sisyphusClient/component/TusdUploadTest.java
@@ -16,9 +16,20 @@ public class TusdUploadTest {
     public void calcFingerprint() throws IOException {
         final Path file = Files.createTempFile("asynchronousFileChannelQuietly", " 1");
         Files.write(file, "Test,test,test".getBytes());
+        final TusdUpload tusUploader = TusdUpload.builder().path(file).build();
+
+        final String fingerprint = tusUploader.calcFingerprint();
+        assertTrue(fingerprint.contains("14"));
+        System.out.println(fingerprint);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void calcFingerprint_nullPointerException() throws IOException {
+        final Path file = Files.createTempFile("asynchronousFileChannelQuietly", " 1");
+        Files.write(file, "Test,test,test".getBytes());
         final TusdUpload tusUploader = TusdUpload.builder().build();
 
-        final String fingerprint = tusUploader.calcFingerprint(file);
+        final String fingerprint = tusUploader.calcFingerprint();
         assertTrue(fingerprint.contains("14"));
         System.out.println(fingerprint);
     }
@@ -28,8 +39,20 @@ public class TusdUploadTest {
     public void generateMetadataQuietly() throws IOException {
         final Path file = Files.createTempFile("generateMetadataQuietly", " 1");
 
+        final TusdUpload tusUploader = TusdUpload.builder().path(file).build();
+        final String metadata = tusUploader.generateMetadataQuietly();
+        final String encodeToString = Base64.getEncoder().encodeToString(file.getFileName().toString().getBytes());
+        assertTrue(metadata.contains(encodeToString));
+        assertTrue(metadata.contains("filename"));
+    }
+
+
+    @Test(expected =  NullPointerException.class)
+    public void generateMetadataQuietly_nullPointerException() throws IOException {
+        final Path file = Files.createTempFile("generateMetadataQuietly", " 1");
+
         final TusdUpload tusUploader = TusdUpload.builder().build();
-        final String metadata = tusUploader.generateMetadataQuietly(file);
+        final String metadata = tusUploader.generateMetadataQuietly();
         final String encodeToString = Base64.getEncoder().encodeToString(file.getFileName().toString().getBytes());
         assertTrue(metadata.contains(encodeToString));
         assertTrue(metadata.contains("filename"));
@@ -39,9 +62,9 @@ public class TusdUploadTest {
     public void asynchronousFileChannelQuietly() throws IOException {
 
         final Path file = Files.createTempFile("asynchronousFileChannelQuietly", " 1");
-        final TusdUpload tusUploader = TusdUpload.builder().build();
+        final TusdUpload tusUploader = TusdUpload.builder().path(file).build();
 
-        final AsynchronousFileChannel asynchronousFileChannel = tusUploader.asynchronousFileChannelQuietly(file);
+        final AsynchronousFileChannel asynchronousFileChannel = tusUploader.asynchronousFileChannelQuietly();
         assertTrue(asynchronousFileChannel.isOpen());
         asynchronousFileChannel.close();
 
